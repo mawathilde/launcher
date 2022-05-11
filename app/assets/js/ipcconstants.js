1805584b07be641abc372ed514877a53582721dd
Mawathilde <mawathilde@gmail.com>
2022-05-11 10:06:33 +0200

    Customize launcher

--- app/assets/js/ipcconstants.js
+++ app/assets/js/ipcconstants.js
@@ -1,7 +1,7 @@
 // NOTE FOR THIRD-PARTY
 // REPLACE THIS CLIENT ID WITH YOUR APPLICATION ID.
 // SEE https://github.com/dscalzi/HeliosLauncher/blob/master/docs/MicrosoftAuth.md
-exports.AZURE_CLIENT_ID = '1ce6e35a-126f-48fd-97fb-54d143ac6d45'
+exports.AZURE_CLIENT_ID = '4e71065c-a797-4d80-9f61-7183829c07b3'
 // SEE NOTE ABOVE.
 
 
