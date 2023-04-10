f7d82baad768c712fad01052194e0ff6c32b760a
mawathilde <dev@mawathilde.fr>
2023-04-10 09:16:34 +0200

    :bug: Fix populateReleaseNotes url

--- app/assets/js/processbuilder.js
+++ app/assets/js/processbuilder.js
@@ -506,7 +506,7 @@ class ProcessBuilder {
                             val = args[i].replace(argDiscovery, tempNativePath)
                             break
                         case 'launcher_name':
-                            val = args[i].replace(argDiscovery, 'Helios-Launcher')
+                            val = args[i].replace(argDiscovery, 'WizardsStory-Launcher')
                             break
                         case 'launcher_version':
                             val = args[i].replace(argDiscovery, this.launcherVersion)
